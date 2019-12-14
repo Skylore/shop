aba5b50fbf9563e0b7deb436bd84793bc42b68f2
YanPooo <IronEyeee@gmail.com>
2019-12-14 19:12:33 +0200

    User and product_request

--- models/product_request.py
+++ models/product_request.py
@@ -3,3 +3,30 @@ class ProductRequest:
         self.__status = 'active'
         self.__login = login
         self.__product = product
+
+    def getStatus(self):
+        return self.__status
+
+    def getLogin(self):
+        return self.__login
+
+    def getProduct(self):
+        return self.__product
+
+    def setStatus(self, status):
+        self.__status = status
+
+    def setLogin(self, login):
+        self.__login = login
+
+    def setProduct(self, product):
+        self.__product = product
+
+    def switchStatus(self):
+        if self.__status == 'active':
+            self.__status = 'sold'
+        else:
+            self.__status = 'active'
+
+    def __str__(self):
+        return 'Login = {}, Product = {}, Status = {}'.format(self.__login, self.__product, self.__status)
--- models/user.py
+++ models/user.py
@@ -5,3 +5,40 @@ class User:
         self.__password = password
         self.__role = role
         self.__is_blocked = False
+
+    def getName(self):
+        return self.__name
+
+    def getLogin(self):
+        return self.__login
+
+    def getPassword(self):
+        return self.__password
+
+    def getRole(self):
+        return self.__role
+
+    def getBlock(self):
+        return self.__is_blocked
+
+
+    def setName(self, name):
+        self.__name = name
+
+    def setLogin(self, login):
+        self.__login = login
+
+    def setPassword(self, password):
+        self.__password = password
+
+    def setRole(self, role):
+        self.__role = role
+
+    def setBlock(self, block):
+        self.__is_blocked = block
+
+    def __str__(self):
+        return 'Name = {}, Login = {}, Password = {}, Role = {}, {}'.format(self.__name,self.__login,self.__password,self.__role,self.__is_blocked)
+
+    def __eq__(self, other):
+        return self.__name == other.getName()
