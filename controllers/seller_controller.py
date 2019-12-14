ea9ed8343acdf632fabab23c3f8c0cf5995ae5bf
r4bc1 <r4bckagarl@gmail.com>
2019-12-14 21:34:12 +0200

    Romchik SellerController with LOVE

--- controllers/seller_controller.py
+++ controllers/seller_controller.py
@@ -1,3 +1,5 @@
+import random
+
 from models import product
 from db import db
 from exceptions import ProductNotFound
@@ -8,4 +10,41 @@ ProductNotFoundException = ProductNotFound.ProductNotFoundException
 
 
 class SellerController:
-    pass
+    def __init__(self, user):
+        self.__user = user
+
+    def add_product(self, product_name, product_desc):
+        while True:
+            prod_id = random.randint(0, 1000)
+
+            if prod_id not in DataBase.products:
+                prod = Product(prod_id, product_name, product_desc, self.__user.getLogin())
+                DataBase.products[prod_id, prod.getProductId()] = prod
+
+            break
+
+    def remove_product(self, product_id):
+
+        if product_id not in DataBase.products:
+            raise PermissionError
+        elif self.__user.getLogin() != DataBase.products[product_id].getOwner():
+            raise PermissionError
+
+        return DataBase.products.pop(product_id)
+
+    def my_product(self):
+        massivchik = []
+
+        for i in DataBase.products:
+            if DataBase.products[i].getOwner() == self.__user.getLogin():
+                massivchik.append(DataBase.products[i])
+
+    def product_requests(self):
+
+        roma = []
+        # status = active, status.product.owner = self.user.login
+        for i in DataBase.product_requests:
+            if i.getStatus() == 'active' and i.getProduct().getOwner() == self.__user.getLogin():
+                roma.append(i)
+
+        return roma
