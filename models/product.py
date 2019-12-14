0b97b030c9688dca30373af0c9d06a8edf7c962c
power526 <krasivan20021@gmial.com>
2019-12-14 19:28:07 +0200

    vova pidor

--- models/product.py
+++ models/product.py
@@ -4,3 +4,47 @@ class Product:
         self.__product_name = product_name
         self.__product_desc = product_desc
         self.__owner = owner
+
+
+    def getProductId(self):
+        return self.__product_id
+
+
+    def getProductName(self):
+        return self.__product_name
+
+
+    def getProductDesk(self):
+        return self.__product_desc
+
+
+    def getOwner(self):
+        return self.__owner
+
+
+    def setProductId(self, product_id):
+        self.__product_id = product_id
+
+
+    def setProductName(self, product_name):
+        self.__product_name = product_name
+
+
+    def setProductDesk(self, product_desc):
+        self.__product_desc = product_desc
+
+
+    def setOwner(self, owner):
+        self.__owner = owner
+
+
+    def __str__(self):
+        return 'product id: {}, product name: {}, product description: {}, owner: {}'.format(self.__product_id, self.__product_name, self.__product_desc, self.__owner)
+
+
+    def __eq__(self, other):
+        if self.product_id == other.getProductId():
+            return other
+
+
+
--- models/support_request.py
+++ models/support_request.py
@@ -4,3 +4,41 @@ class SupportRequest:
         self.__user_message = user_message
         self.__support_message = ""
         self.__status = 'active'
+
+
+    def getLogin(self):
+        return self.__login
+
+
+    def getUserMessage(self):
+        return self.__user_message
+
+
+    def getSupportMessage(self):
+        return self.__support_message
+
+
+    def getStatus(self):
+        return self.__status
+
+
+    def setLogin(self, login):
+         self.__login = login
+
+
+    def setUserMessage(self, user_message):
+        self.__user_message = user_message
+
+
+    def setSupportMessage(self, support_message,):
+        self.__support_message = support_message
+        self.__status = 'resolved'
+
+
+
+
+
+
+
+
+
